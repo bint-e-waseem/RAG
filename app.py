eb84ccffe4cccb79d8cc8576ac55084c940dd069
Yashfa waseem <yashfawaseem2006@gmail.com>
2026-09-22 10:36:53 +0500

    Refactor app.py for improved readability

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from langchain_community.document_loaders import UnstructuredURLLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain.chains.retrieval import create_retrieval_chain
+from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents.stuff import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 
