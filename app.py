b9db0ea4fa7f77ddca5fcd9c5f9aca1a1ee7738f
Yashfa waseem <yashfawaseem2006@gmail.com>
2026-09-22 10:31:24 +0500

    Refactor imports and update function paths

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from langchain_community.document_loaders import UnstructuredURLLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain.chains import create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains.retrieval import create_retrieval_chain
+from langchain.chains.combine_documents.stuff import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 
 from dotenv import load_dotenv
